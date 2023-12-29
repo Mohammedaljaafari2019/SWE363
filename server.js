0e05e3ecce21f470684887735dbcf61b06d4a7b3
Mohammedaljaafari2019 <124896261+Mohammedaljaafari2019@users.noreply.github.com>
2023-12-29 18:38:44 +0300

    html-1 modifcation

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-var http = require("http");
-var fs = require('fs');
-const host ="localhost";
-const PORT=8080; 
+// var http = require("http");
+// var fs = require('fs');
+// const host ="localhost";
+// const PORT=8080; 
 
-const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/index");
-    res.writeHead(200);
-    res.end("My first server!");
-};
+// const requestListener = function (req, res) {
+//     res.setHeader("Content-Type", "text/index");
+//     res.writeHead(200);
+//     res.end("My first server!");
+// };
 
 
-const server = http.createServer(requestListener);
-server.listen(PORT, host, () => {
-    console.log(`Server is running on http://${host}:${PORT}`);
-});
+// const server = http.createServer(requestListener);
+// server.listen(PORT, host, () => {
+//     console.log(`Server is running on http://${host}:${PORT}`);
+// });
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('./form'))
+app.get('/compleform',(req,res)=>{
+    res.sendFile(path.join(__dirname, './form/compleform.html'));
+})
+app.post('/done',(req,res)=>{
+    res.sendfile(path.join(__dirname,'thankyou.html'))
+})
+
+app.listen(5000,()=>{
+    console.log('welcome')
+})
